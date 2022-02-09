2979d42f2da52b3899c6b0b47cd6342a2a80c2b4
arnisafazla <arnisa.fazla@ug.bilkent.edu.tr>
2022-02-09 23:02:08 +0000

    conditional batch normalization in generator is done. probably needs parameter tuning

--- models/generator_models/norm_generator.py
+++ models/generator_models/norm_generator.py
@@ -3,20 +3,60 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import utils
   
+class ConditionalBatchNorm(layers.Layer):
+  def build(self, input_shape):
+    self.seq_len = 69
+    self.n_classes = 6
+    self.gamma = self.add_weight(shape=[self.n_classes, self.seq_len], 
+        initializer='zeros', trainable=True, name='gamma')
+    self.beta = self.add_weight(shape=[self.n_classes, self.seq_len], 
+        initializer='zeros', trainable=True, name='beta')
+    self.moving_mean = self.add_weight(shape=[1, self.seq_len],
+        initializer='zeros', trainable=False, name='moving_mean')
+    self.moving_var = self.add_weight(shape=[1, self.seq_len], 
+        initializer='zeros', trainable=False, name='moving_var')
+    self.alpha = 0.99  # alpha is the decay parameter for exponential moving average
+    # it is 0.99 in keras.layers.BatchNormalization so I use it too.
+    self.eps = 0.00001  # only for prevent dividing by 0. keras.layers.BatchNormalization use 0.001, I use smaller for safer
+  def call(self, inputs, training=False):
+    x, labels = tf.split(inputs, [self.seq_len, 1], axis=1)
+    labels = tf.cast(labels, tf.int32)
+    beta = tf.gather(self.beta, labels)
+    # print(beta)
+    beta = tf.reshape(beta, (-1, beta.shape[-1]))
+    # print(beta)
+    gamma = tf.gather(self.gamma, labels)
+    gamma = tf.reshape(gamma, (-1, gamma.shape[-1]))
+    if training:
+      mean, var = tf.nn.moments(x, axes=(0), keepdims=True)
+      self.moving_mean.assign(self.alpha * self.moving_mean + (1-self.alpha)*mean)
+      self.moving_var.assign(self.alpha * self.moving_var + (1-self.alpha)*var)
+      # mean = tf.repeat(mean, labels.shape[0], axis=0)
+      # var = tf.repeat(var, labels.shape[0], axis=0)
+      # print(x.shape, mean.shape, var.shape, beta.shape, gamma.shape)
+      output = tf.nn.batch_normalization(x, mean, var, beta, gamma, self.eps)
+    else:
+      output = tf.nn.batch_normalization(x, self.moving_mean, self.moving_var, beta, gamma, self.eps)
+    return output
+  def compute_output_shape(self, input_shape):
+    return (None, self.seq_len)
+  
 def define_generator(config):
   init = keras.initializers.RandomNormal(stddev=config['init_std'])
   in_label = layers.Input(shape=(1,), name='label_input')
-  li = layers.Embedding(config['n_classes'], config['in_shape'][1], name='label_embedding')(in_label)
-  li = layers.Reshape((-1,))(li)
-  li = layers.RepeatVector(config['in_shape'][0], name='label_repeat')(li)
+  # li = layers.CategoryEncoding(num_tokens=config['n_classes'], output_mode="one_hot", name='one-hot')(in_label)
+  li = layers.RepeatVector(config['in_shape'][0], name='repeat')(in_label)
 
   in_lat = layers.Input(shape=(config['latent_dim'],), name='seq_input')
   lat = layers.Dense(config['in_shape'][0] * config['in_shape'][1], name='lat_upsample')(in_lat)
   lat = layers.Reshape((config['in_shape'][0], config['in_shape'][1]))(lat)
-  merge = keras.layers.Concatenate(name='concatenate', axis=2)([li, lat])
+  # merge = keras.layers.Concatenate(name='concatenate', axis=2)([li, lat])
 
-  hidden1 = layers.LSTM(config['in_shape'][1], name='hidden1', return_sequences=True, kernel_initializer=init)(merge)
-  out_layer = layers.LSTM(config['in_shape'][1], name='out_LSTM', return_sequences=True, kernel_initializer=init)(hidden1)
- 
-  model = keras.Model([in_label, in_lat], out_layer, name='generator')
+  hidden1 = layers.LSTM(config['in_shape'][1], name='hidden1', return_sequences=True, kernel_initializer=init)(lat)
+  merged = layers.Concatenate(axis=2, name='concatenate')([hidden1, li])
+  hidden1 = layers.TimeDistributed(ConditionalBatchNorm(name='conditional_batch_norm'))(merged)
+  hidden2 = layers.LSTM(config['in_shape'][1], name='out_LSTM', return_sequences=True, kernel_initializer=init)(hidden1)
+  merged2 = layers.Concatenate(axis=2, name='concatenate2')([hidden2, li])
+  out = layers.TimeDistributed(ConditionalBatchNorm(name='conditional_batch_norm2'))(merged2)
+  model = keras.Model([in_label, in_lat], out, name='generator')
   return model
